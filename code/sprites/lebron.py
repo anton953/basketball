87d2d5cda55ca7f325e47f30947c7b93d012396c
EgorKa-programmister <egorkasianov@icloud.com>
2025-01-31 14:42:09 +0300

    lebron_animat

--- code/sprites/lebron.py
+++ code/sprites/lebron.py
@@ -9,7 +9,7 @@ from game_object.game_functions import load_image
 class Lebron(pygame.sprite.Sprite):
 
     def __init__(self, game, ai_settings, *group):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. 
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
         # Это очень важно!!!
         super().__init__(*group)
 
@@ -21,27 +21,28 @@ class Lebron(pygame.sprite.Sprite):
 
         self.rect.x = 350
         self.rect.y = 350
-        
+
         self.moving_forward = False
         self.moving_back = False
         self.moving_right = False
         self.moving_left = False
-        
+
         self.speed = ai_settings.speed
 
         self.ball_status = False
 
-        self.facing = 'back'
+        self.facing = 'b'
 
         self.x_change = 0
         self.y_change = 0
 
         self.fps_cnt = 0
 
+        self.ball_cnt = 1
+
         # включить или выключить анимацию
         self.ball_animatin = False
-     
-   
+
     def update(self):
         self.movement()
 
@@ -56,64 +57,47 @@ class Lebron(pygame.sprite.Sprite):
     def movement(self):
         if self.moving_right and self.rect.right < self.game.screen.get_width():
             self.x_change += self.speed
-            self.facing = 'right'
+            self.facing = 'r'
         if self.moving_left and self.rect.left > 0:
             self.x_change -= self.speed
-            self.facing = 'left'
-    
+            self.facing = 'l'
+
         if self.moving_back and self.rect.bottom < self.game.screen.get_height():
             self.y_change += self.speed
-            self.facing = 'back'
+            self.facing = 'f'
         if self.moving_forward and self.rect.top > 0:
             self.y_change -= self.speed
-            self.facing = 'front'
+            self.facing = 'b'
 
     def animate(self):
-        down_animations = [load_image("lebron/lebron_animaton_static/lebron_f_1.png").convert_alpha(),
-                           load_image("lebron/lebron_animaton_static/lebron_f_2.png").convert_alpha()]
-        
-        up_animations = [load_image("lebron/lebron_animaton_static/lebron_b_1.png").convert_alpha(),
-                         load_image("lebron/lebron_animaton_static/lebron_b_2.png").convert_alpha()]
-        
-        right_animations = [load_image("lebron/lebron_animaton_static/lebron_r_1.png").convert_alpha(),
-                            load_image("lebron/lebron_animaton_static/lebron_r_2.png").convert_alpha()]
-        
-        left_animations = [load_image("lebron/lebron_animaton_static/lebron_l_1.png").convert_alpha(),
-                           load_image("lebron/lebron_animaton_static/lebron_l_2.png").convert_alpha()]
-
         if self.fps_cnt == 60:
-                self.fps_cnt = 0
+            self.fps_cnt = 0
 
-        if self.facing == 'back':
-            if self.moving_back:
-                self.image = down_animations[0]
-            elif self.fps_cnt == 0:
-                self.image = down_animations[0]
-            elif self.fps_cnt == 50:
-                self.image = down_animations[1]
+        if self.check_move() and self.ball_status:
+            self.ball_anim()
+            self.image = load_image(f'lebron/lebron_animaton_ball/lebron_ball_{self.facing}_{self.ball_cnt}.png')
+        elif self.check_move():
+            self.image = load_image(f'lebron/lebron_animaton_static/lebron_{self.facing}_1.png')
+        elif self.ball_status:
+            self.ball_anim()
+            self.image = load_image(f'lebron/lebron_animaton_ball/lebron_ball_{self.facing}_{self.ball_cnt}.png')
+        else:
+            self.image = load_image(f'lebron/lebron_animaton_static/lebron_{self.facing}_{'
+            1
+            ' if 50 > self.fps_cnt >= 0 else '
+            2
+            '}.png')
 
-        elif self.facing == 'front':
-            if self.moving_forward:
-                self.image = up_animations[0]
-            elif self.fps_cnt == 0:
-                self.image = up_animations[0]
-            elif self.fps_cnt == 50:
-                self.image = up_animations[1]
+            self.fps_cnt += 1
 
-        elif self.facing == 'right':
-            if self.moving_right:
-                self.image = right_animations[0]
-            elif self.fps_cnt == 0:
-                self.image = right_animations[0]
-            elif self.fps_cnt == 50:
-                self.image = right_animations[1]
+        def check_move(self):
+            if self.moving_forward or self.moving_back or self.moving_left or self.moving_right:
+                return True
+            return False
 
-        elif self.facing == 'left':
-            if self.moving_left:
-                self.image = left_animations[0]
-            elif self.fps_cnt == 0:
-                self.image = left_animations[0]
-            elif self.fps_cnt == 50:
-                self.image = left_animations[1]
+        def ball_anim(self):
+            if self.fps_cnt % 10 == 0:
+                self.ball_cnt += 1
 
-        self.fps_cnt += 1
+            if self.ball_cnt == 5:
+                self.ball_cnt = 1
